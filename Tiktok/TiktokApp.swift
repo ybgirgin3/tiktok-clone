6e3210407f6fb0bb446d2be54300fbb917040b07
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2024-07-28 23:29:40 +0300

    Initial Commit

---
+++ Tiktok/TiktokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiktokApp.swift
+//  Tiktok
+//
+//  Created by Yusuf Berkay Girgin on 28.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TiktokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
