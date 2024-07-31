32a609fe197f69b5c6876ea7168fde14de420982
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2024-08-01 00:23:33 +0300

    explore cell created

---
+++ Tiktok/Core/Search/ExploreView.swift
@@ -0,0 +1,30 @@
+//
+//  ExploreView.swift
+//  Tiktok
+//
+//  Created by Yusuf Berkay Girgin on 1.08.2024.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack (spacing: 16) {
+                    ForEach(0 ..< 20) { user in
+                        UserCell()
+                            .padding(.horizontal)
+                    }
+                }
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(.top)
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
---
+++ Tiktok/Core/Search/UserCell.swift
@@ -0,0 +1,35 @@
+//
+//  UserCell.swift
+//  Tiktok
+//
+//  Created by Yusuf Berkay Girgin on 1.08.2024.
+//
+
+import SwiftUI
+
+struct UserCell: View {
+    var body: some View {
+        HStack (spacing: 12) {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 48, height: 48)
+                .foregroundStyle(Color(.systemGray5))
+            VStack (alignment: .leading) {
+                Text("bekocan_tiktok")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                
+                Text("Bekocan")
+                    .font(.footnote)
+            }
+            
+            Spacer()
+            
+            
+        }
+    }
+}
+
+#Preview {
+    UserCell()
+}
